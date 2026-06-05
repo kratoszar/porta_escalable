e59f80adb13861757a0c33f218de8218c6e201b3
Alberto Rosario Zarco <albertozarco57@gmail.com>
2026-06-05 13:35:28 -0600

    modificacion con datos personales

---
+++ src/utils/datosporta.js
@@ -0,0 +1,20 @@
+export const misProyectos = [
+  {
+    imagenC: "/imaages/fulls/01.jpg", 
+    imagenM: "/images/thumbs/01.jpg",
+    titulo: "ForaneosHub",
+    descripcion: "Plataforma web y móvil diseñada para gestionar el alojamiento de estudiantes universitarios."
+  },
+  {
+    imagenC: "/images/fulls/02.jpg",
+    imagenM: "/images/thumbs/02.jpg",
+    titulo: "AdoptaPet",
+    descripcion: "Plataforma interactiva para la adopción de mascota."
+  },
+  {
+    imagenC: "/images/fulls/03.jpg",
+    imagenM: "/images/thumbs/03.jpg",
+    titulo: "Sound",
+    descripcion: "Plataforma para la carga y visualización de archivos multimedia como fotos y videos."
+  }
+];
