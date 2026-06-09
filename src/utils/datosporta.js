14d298c8b844102a56ec0647ddd1df8fa32f8fcc
Alberto Rosario Zarco <albertozarco57@gmail.com>
2026-06-09 14:05:26 -0600

    agrego correcion de links a repositorios

--- src/utils/datosporta.js
+++ src/utils/datosporta.js
@@ -1,18 +1,18 @@
 export const misProyectos = [
   {
-    imagenC: "/imaages/fulls/01.jpg", 
+    linkGithub: "https://github.com/kratoszar/jelp.git", 
     imagenM: "/images/thumbs/01.jpg",
     titulo: "ForaneosHub",
     descripcion: "Plataforma web y móvil diseñada para gestionar el alojamiento de estudiantes universitarios."
   },
   {
-    imagenC: "/images/fulls/02.jpg",
+    linkGithub: "https://github.com/kratoszar/Astrotuto.git",
     imagenM: "/images/thumbs/02.jpg",
-    titulo: "AdoptaPet",
-    descripcion: "Plataforma interactiva para la adopción de mascota."
+    titulo: "Tutorial de astro",
+    descripcion: "Plataforma donde se creo un tutorial de Astro."
   },
   {
-    imagenC: "/images/fulls/03.jpg",
+    linkGithub: "https://github.com/kratoszar/Sound.git",
     imagenM: "/images/thumbs/03.jpg",
     titulo: "Sound",
     descripcion: "Plataforma para la carga y visualización de archivos multimedia como fotos y videos."
