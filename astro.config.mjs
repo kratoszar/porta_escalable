3697feea3f386a03e68f7462dd6503d86f643b9d
Alberto Rosario Zarco <albertozarco57@gmail.com>
2026-06-19 12:45:56 -0600

    limpieza del config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,7 @@ import emdash from 'emdash/astro';
 import react from '@astrojs/react';
 import { sqlite, libsql } from 'emdash/db';
 import { emdashSmtp } from 'emdash-smtp';
-
 const isProd = !!process.env.TURSO_URL;
-
 export default defineConfig({
   site: process.env.URL || 'https://portafoliotu.netlify.app',
   output: 'server',
@@ -36,13 +34,6 @@ export default defineConfig({
         'use-sync-external-store/shim',
         'use-sync-external-store/shim/with-selector'
       ]
-    },
-    define: {
-      'process.env.EMDASH_EMAIL_FROM': JSON.stringify('albertozarco57@gmail.com'),
-      'process.env.EMDASH_SMTP_HOST': JSON.stringify('smtp.gmail.com'),
-      'process.env.EMDASH_SMTP_PORT': JSON.stringify('587'),
-      'process.env.EMDASH_SMTP_USER': JSON.stringify('albertozarco57@gmail.com'),
-      'process.env.EMDASH_SMTP_PASS': JSON.stringify('hyqceqqadmcqakuq')
     }
   }
 });
